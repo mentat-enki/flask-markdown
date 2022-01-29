8318d13c4f782da65d950a307f52b1b91805d637
Scott Ziegler <aslan235@gmail.com>
2022-01-29 12:31:44 -0500

    bumps version to 0.4.1 for warning fix

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Markdown',
-    version='0.4',
+    version='0.4.1',
     url='http://github.com/dcolish/flask-markdown',
     license='BSD',
     author='Dan Colish',
