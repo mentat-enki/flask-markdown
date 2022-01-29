f00fb0a922bebc663ceae1b603295111134c9c1f
Scott Ziegler <aslan235@gmail.com>
2022-01-29 13:08:00 -0500

    modifies the license and setup to reflect the fork

--- setup.py
+++ setup.py
@@ -9,20 +9,17 @@ Links
 `````
 
 * `documentation <http://packages.python.org/Flask-Markdown>`_
-* `development version
-  <http://github.com/dcolish/flask-markdown/zipball/master#egg=Flask-Markdown-dev>`_
 
 """
 from setuptools import setup
 
-
 setup(
     name='Flask-Markdown',
     version='0.4.1',
-    url='http://github.com/dcolish/flask-markdown',
+    url='https://github.com/mentat-enki/flask-markdown',
     license='BSD',
-    author='Dan Colish',
-    author_email='dcolish@gmail.com',
+    author='Dan Colish & Scott Ziegler',
+    author_email='aslan235@gmail.com',
     description='Small extension to make using markdown easy',
     long_description=__doc__,
     packages=['flaskext'],
